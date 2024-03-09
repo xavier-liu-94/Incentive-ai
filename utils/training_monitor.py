3d1a617acc8b5e5abefcfb5e4e580b841c7879d4
xavierLiu <lxr_orz@outlook.com>
2024-03-09 12:08:26 +0800

    bug fix

--- utils/training_monitor.py
+++ utils/training_monitor.py
@@ -15,23 +15,30 @@ class TrainMonitor():
             h.remove()
         self.writer.close()
 
-    def register(self, module: torch.nn.Module, namespace: str):
-        for n, m in module.named_children():
-            self.handles.append(
-                m.register_full_backward_hook(self.get_back_hook(n, namespace))
-            )
+    def register_backward(self, module: torch.nn.Module, namespace: str):
+
+        def iter_regist(module: torch.nn.Module, prefix=[]):
+            for n, m in module.named_children():
+                if isinstance(m, (torch.nn.ModuleList, torch.nn.ModuleDict)):
+                    iter_regist(m, prefix+[n])
+                else:
+                    self.handles.append(
+                        m.register_full_backward_hook(self.get_back_hook(".".join(prefix + [n]), namespace))
+                    )
+
+        iter_regist(module)
 
     def get_back_hook(self, name: str, name_space: str=""):
         full_name = name_space + "-" + name
         def hook(module, grad_input, grad_output):
             for idx, t in enumerate(grad_output):
                 if t is not None:
-                    m, s = torch.std_mean(t)
+                    s, m = torch.std_mean(t)
                     self.writer.add_scalars(self.monitor_name, {full_name+"-grad_out-mean_"+str(idx): m.detach().cpu().numpy()})
                     self.writer.add_scalars(self.monitor_name, {full_name+"-grad_out-std_"+str(idx): s.detach().cpu().numpy()})
             for idx, t in enumerate(grad_input):
                 if t is not None:
-                    m, s = torch.std_mean(t)
+                    s, m = torch.std_mean(t)
                     self.writer.add_scalars(self.monitor_name, {full_name+"-grad_in-mean_"+str(idx): m.detach().cpu().numpy()})
                     self.writer.add_scalars(self.monitor_name, {full_name+"-grad_in-std_"+str(idx): s.detach().cpu().numpy()})
             
